fd14b09948337ab25e74397c4c39bbfba9b6d55d
Arieswarrior888 <arieswarrior888games@gmail.com>
2022-01-06 10:26:15 -0600

    Add project files.

---
+++ LandingPage.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Shopping_List_Tracker
+{
+    partial class LandingPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.btnRecipe = new System.Windows.Forms.Button();
+            this.grdPlan = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPlan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(341, 39);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(95, 46);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.Location = new System.Drawing.Point(349, 123);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(75, 23);
+            this.btnCalendar.TabIndex = 2;
+            this.btnCalendar.Text = "Calendar";
+            this.btnCalendar.UseVisualStyleBackColor = true;
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.Location = new System.Drawing.Point(656, 123);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(75, 23);
+            this.btnInventory.TabIndex = 3;
+            this.btnInventory.Text = "Inventory";
+            this.btnInventory.UseVisualStyleBackColor = true;
+            // 
+            // btnRecipe
+            // 
+            this.btnRecipe.Location = new System.Drawing.Point(54, 123);
+            this.btnRecipe.Name = "btnRecipe";
+            this.btnRecipe.Size = new System.Drawing.Size(75, 23);
+            this.btnRecipe.TabIndex = 4;
+            this.btnRecipe.Text = "Recipe";
+            this.btnRecipe.UseVisualStyleBackColor = true;
+            this.btnRecipe.Click += new System.EventHandler(this.btnRecipe_Click);
+            // 
+            // grdPlan
+            // 
+            this.grdPlan.AllowUserToAddRows = false;
+            this.grdPlan.AllowUserToDeleteRows = false;
+            this.grdPlan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPlan.Location = new System.Drawing.Point(185, 207);
+            this.grdPlan.Name = "grdPlan";
+            this.grdPlan.ReadOnly = true;
+            this.grdPlan.Size = new System.Drawing.Size(462, 205);
+            this.grdPlan.TabIndex = 5;
+            // 
+            // LandingPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdPlan);
+            this.Controls.Add(this.btnRecipe);
+            this.Controls.Add(this.btnInventory);
+            this.Controls.Add(this.btnCalendar);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "LandingPage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LandingPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdPlan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnCalendar;
+        private System.Windows.Forms.Button btnInventory;
+        private System.Windows.Forms.Button btnRecipe;
+        private System.Windows.Forms.DataGridView grdPlan;
+    }
+}
+
---
+++ LandingPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_List_Tracker
+{
+    public partial class LandingPage : Form
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+
+        public void setControls()
+        {
+            
+        }
+
+        private void LandingPage_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            this.setControls();
+        }
+
+        private void btnRecipe_Click(object sender, EventArgs e)
+        {
+            Recipe frmRecipe = new Recipe();
+            frmRecipe.ShowDialog();
+        }
+    }
+}
